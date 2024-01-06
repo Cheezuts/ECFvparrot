e358e5fb2fe13eef2cd842776d07a65e9e7f5400
cyril <telliercyril@gmail.com>
2024-01-06 16:48:31 +0100

    Header + Navbar + Footer + CSS + Bootstrap

---
+++ app/includes/Navbar.php
@@ -0,0 +1,31 @@
+<div class="container-fluid sticky-top">
+    <nav class="navbar navbar-expand-lg p-0">
+        <a href="index.php" class="navbar-brand">
+            <h1 class="headline">Accueil</h1>
+        </a>
+        <button type="button" class="navbar-toggler ms-auto me-0" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ms-auto">
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link active">Voitures</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Avis</a>
+                </li>
+
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Admin</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
